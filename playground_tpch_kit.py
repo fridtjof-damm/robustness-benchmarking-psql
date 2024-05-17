b2aa435eda81f711e45451f597bdfa77e21b07ff
fridtjof-damm <soenke.f.damm@campus.tu-berlin.de>
2024-05-17 15:14:48 +0200

    tpch-kit playground

--- playground_tpch_kit.py
+++ playground_tpch_kit.py
@@ -1,4 +1,6 @@
 import duckdb
 
 cursor = duckdb.connect(".open tpch-kit-v.duckdb")
-print(cursor.execute("PRAGMA tpch(22)").df())
+
+print(cursor.execute('tpch-kit/dbgen/queries/22.sql').df())
+# print(cursor.execute("PRAGMA tpch(22)").df())
