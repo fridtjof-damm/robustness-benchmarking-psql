e1eb295fb1d55b7cc38c4ccf7126ba2f058d0227
fridtjof-damm <soenke.f.damm@campus.tu-berlin.de>
2024-06-12 19:14:25 +0200

    [Update] dummy data gen

--- src/analysis/dummy/load_dummy_data.py
+++ src/analysis/dummy/load_dummy_data.py
@@ -1,22 +1,20 @@
 import duckdb
-import analysis.dummy.dummy_data_gen as ddg
-import json
+import dummy_data_gen as ddg
+import pandas as pd
+#import json
 
-cursor = duckdb.connect('dummy_db.duckdb')
+cursor = duckdb.connect('resources/db/dummy_db.duckdb')
 # already created numbers table 
-#cursor.sql("CREATE TABLE numbers (number INTEGER);")
-#tables = cursor.sql("SHOW TABLES;").fetchall()
+cursor.sql("DROP TABLE IF EXISTS numbers;")
+cursor.sql("CREATE TABLE numbers (number INTEGER);")
+tables = cursor.sql("SHOW TABLES;").fetchall()
 #print(f'SUCCESS CREATING: {tables}')
-#print(tables)
-#data = ddg.dummygen()
-#l = len(data) 
-#for i,val in enumerate(data):
-#    cursor.sql(f"INSERT INTO numbers (number) VALUES ({val});")
-#    if (i+1) // 5000000 != i // 5000000:
-#        print(f"INSERTED {i+1} ITEMS out of {len(data)}")
-#print('success')
-#res = cursor.sql("FROM numbers LIMIT 20;").fetchall()
-#print(res)
-#profile = cursor.sql("EXPLAIN SELECT COUNT(*) FROM numbers;").fetchall()
-#print(profile)
-cursor.sql("CREATE INDEX n_idx ON numbers (number);")
+print(tables)
+data = ddg.dummygen()
+df = pd.DataFrame(data)
+cursor.sql("INSERT INTO numbers (number) SELECT * FROM df;")
+
+#print(cursor.sql("SELECT COUNT(*) FROM numbers GROUP BY number;").fetchall())
+
+#cursor.sql("CREATE INDEX n_idx ON numbers (number);")
+
