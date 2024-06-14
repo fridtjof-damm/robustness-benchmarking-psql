f2c24289e9bddc0fd7ff1dac579a3103e042dc5a
fridtjof-damm <soenke.f.damm@campus.tu-berlin.de>
2024-06-14 13:17:21 +0200

    [Add] profiling dummy query plans (json format)

--- src/analysis/dummy/load_dummy_data.py
+++ src/analysis/dummy/load_dummy_data.py
@@ -1,20 +1,13 @@
 import duckdb
 import dummy_data_gen as ddg
 import pandas as pd
-#import json
 
 cursor = duckdb.connect('resources/db/dummy_db.duckdb')
-# already created numbers table 
+# clean up before creating table
 cursor.sql("DROP TABLE IF EXISTS numbers;")
 cursor.sql("CREATE TABLE numbers (number INTEGER);")
-tables = cursor.sql("SHOW TABLES;").fetchall()
-#print(f'SUCCESS CREATING: {tables}')
-print(tables)
+# get the dummy data
 data = ddg.dummygen()
 df = pd.DataFrame(data)
 cursor.sql("INSERT INTO numbers (number) SELECT * FROM df;")
-
-#print(cursor.sql("SELECT COUNT(*) FROM numbers GROUP BY number;").fetchall())
-
 #cursor.sql("CREATE INDEX n_idx ON numbers (number);")
-
