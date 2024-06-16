bf26591bc9880674516389f6a86c6bf660480da2
fridtjof-damm <soenke.f.damm@campus.tu-berlin.de>
2024-06-16 13:52:01 +0200

    [Update] Created Index on numbers table

--- src/analysis/dummy/load_dummy_data.py
+++ src/analysis/dummy/load_dummy_data.py
@@ -5,9 +5,11 @@ import pandas as pd
 cursor = duckdb.connect('resources/db/dummy_db.duckdb')
 # clean up before creating table
 cursor.sql("DROP TABLE IF EXISTS numbers;")
+cursor.sql("DROP INDEX IF EXISTS n_idx;")
+# create table
 cursor.sql("CREATE TABLE numbers (number INTEGER);")
 # get the dummy data
 data = ddg.dummygen()
 df = pd.DataFrame(data)
 cursor.sql("INSERT INTO numbers (number) SELECT * FROM df;")
-#cursor.sql("CREATE INDEX n_idx ON numbers (number);")
+cursor.sql("CREATE INDEX n_idx ON numbers (number);")
