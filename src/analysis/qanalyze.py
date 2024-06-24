56a8963f5ff5086d84cb56abe358a1c0fe21ccb9
fridtjof-damm <soenke.f.damm@campus.tu-berlin.de>
2024-06-24 11:53:55 +0200

    [Add] func for psql profiling

--- src/analysis/qanalyze.py
+++ src/analysis/qanalyze.py
@@ -1,9 +1,23 @@
 # https://stackoverflow.com/questions/5243596/python-sql-query-string-formatting
 import duckdb
+import psycopg2 as pg 
 
-cursor = duckdb.connect('dummy_db.duckdb')
-# mit Q9 ausprobieren !!
-cursor.execute("PRAGMA enable_profiling = 'json'")
-cursor.execute(f"PRAGMA profiling_output='{'analysis/dummy_with_index.json'}';")
-cursor.execute("SELECT COUNT(*) FROM numbers;")
-cursor.execute("PRAGMA disable_profiling;")
+db_params = {
+    'database': "dummydb",
+    'user':'fridtjofdamm',
+    'password':'',
+    'host':'localhost',
+    'port':'5432'
+}
+
+def duckdb_profiling():
+    cursor = duckdb.connect('dummy_db.duckdb')
+    # mit Q9 ausprobieren !!
+    cursor.execute("PRAGMA enable_profiling = 'json'")
+    cursor.execute(f"PRAGMA profiling_output='{'analysis/dummy_with_index.json'}';")
+    cursor.execute("SELECT COUNT(*) FROM numbers;")
+    cursor.execute("PRAGMA disable_profiling;")
+
+def psql_profiling():
+    conn = pg.connect(**db_params)
+    
