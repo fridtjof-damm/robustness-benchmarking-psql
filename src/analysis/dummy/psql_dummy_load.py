a7903e9911137efb4dd129fe5e6fe47fcfb2c989
fridtjof-damm <soenke.f.damm@campus.tu-berlin.de>
2024-06-24 11:49:33 +0200

    [Add] load python list data into psql table

--- src/analysis/dummy/psql_dummy_load.py
+++ src/analysis/dummy/psql_dummy_load.py
@@ -1,27 +1,32 @@
 import psycopg2 as pg 
-from psycopg2.extras import execute_values
 import dummy_data_gen as dd 
+# define db parameters
+db_params = {
+    'database': "dummydb",
+    'user':'fridtjofdamm',
+    'password':'',
+    'host':'localhost',
+    'port':'5432'
+}
+
 # connection
-conn = pg.connect(
-        database="dummydb",
-        user='fridtjofdamm',
-        password='',
-        host='localhost',
-        port='5432'
-)   
+conn = pg.connect(**db_params)
 # cursor
 cur = conn.cursor()
+#cur.execute('DROP TABLE IF EXISTS numbers;')
 cur.execute('''CREATE TABLE IF NOT EXISTS numbers(
             number integer
             ) ;''')
-# get dummmy data and insert into
-dataset = dd.dummygen()
+# get dummmy data and insert into table
+#dataset = dd.dummygen2tuples()
 
-for d, i in enumerate(dataset):
-    print(i)
-    #cur.executemany("INSERT INTO numbers (number) VALUES (%s);", (i[1],))
+#insert_stmt = 'INSERT INTO numbers (number) VALUES %s;'
+#execute_values(cur, insert_stmt, dataset)
+#conn.commit()
+# check for correct insertion
+cur.execute("SELECT COUNT(*) as cnt, number FROM numbers GROUP BY number;")
+res = cur.fetchall()
+print(res)
 
-#cur.execute("SELECT COUNT(*) FROM numbers;")
-#print(cur)
 cur.close()
 conn.close()
