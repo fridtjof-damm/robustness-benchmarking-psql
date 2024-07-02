cc76498617d5580f15a848955ba5ae197d15f359
fridtjof-damm <soenke.f.damm@campus.tu-berlin.de>
2024-07-02 13:35:25 +0200

    [update] tpch data load psql

--- src/utils/psql_tpch_setup.py
+++ src/utils/psql_tpch_setup.py
@@ -23,10 +23,16 @@ def load_tbl_data():
     conn = pg.connect(**db_params)
     cur = conn.cursor()
     tables = ['customer.tbl','lineitem.tbl','nation.tbl','orders.tbl','part.tbl','partsupp.tbl','region.tbl','supplier.tbl']
-    for table in tables:
-        file_path = f'resources/db/postgres/tpch/{table}'
-        with open(file_path, encoding='UTF-8', mode='r') as table_file:
-            table_data = table_file.read()
-            table_name = table[:-4]
-            sql = "COPY %s FROM %s "
-            #### to be continued ####
+    try: 
+        for table in tables:
+            file_path = f'resources/db/postgres/tpch/{table}'
+            with open(file_path, encoding='UTF-8', mode='r') as table_file:
+                table_name = table[:-4]
+                sql = f"COPY {table_name} FROM STDIN WITH FORMAT CSV, DELIMITER '|', HEADER FALSE;"
+                cur.copy_expert(sql, table_file)
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        print(f"Error loading table data: {e}")
+                #### to be continued ####
+#load_tbl_data()
