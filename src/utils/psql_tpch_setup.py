f943d1801212735b6790a811a9cb333a736e7cd1
fridtjof-damm <soenke.f.damm@campus.tu-berlin.de>
2024-07-02 17:41:50 +0200

    [add] tpch psql data load

--- src/utils/psql_tpch_setup.py
+++ src/utils/psql_tpch_setup.py
@@ -1,7 +1,7 @@
 import psycopg2 as pg
 
 db_params = {
-    'database': "dummydb",
+    'database': 'dummydb',
     'user':'fridtjofdamm',
     'password':'',
     'host':'localhost',
@@ -12,6 +12,7 @@ def load_tpch_schema():
     conn = pg.connect(**db_params)
     conn.autocommit = True
     cur = conn.cursor()
+    ## execption handling
     with open('resources/db/postgres/tpch/schema_tpch.sql', encoding='UTF-8', mode='r') as schema_file:
         schema = schema_file.read()
         cur.execute(schema)
@@ -28,7 +29,7 @@ def load_tbl_data():
             file_path = f'resources/db/postgres/tpch/{table}'
             with open(file_path, encoding='UTF-8', mode='r') as table_file:
                 table_name = table[:-4]
-                sql = f"COPY {table_name} FROM STDIN WITH FORMAT CSV, DELIMITER '|', HEADER FALSE;"
+                sql = f"COPY {table_name} FROM STDIN WITH (FORMAT CSV, DELIMITER '|', HEADER OFF, ENCODING 'UTF8');"
                 cur.copy_expert(sql, table_file)
         conn.commit()
     except Exception as e:
