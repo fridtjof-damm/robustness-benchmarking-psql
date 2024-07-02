cec834420cdbd957c71732d87f09e63b6a935ee3
fridtjof-damm <soenke.f.damm@campus.tu-berlin.de>
2024-07-02 18:26:14 +0200

    [update] refactor !! STILL COPY FAILURE

--- src/utils/psql_tpch_setup.py
+++ src/utils/psql_tpch_setup.py
@@ -1,7 +1,7 @@
 import psycopg2 as pg
 
 db_params = {
-    'database': 'dummydb',
+    'database': "dummydb",
     'user':'fridtjofdamm',
     'password':'',
     'host':'localhost',
@@ -23,17 +23,16 @@ def load_tpch_schema():
 def load_tbl_data():
     conn = pg.connect(**db_params)
     cur = conn.cursor()
-    tables = ['customer.tbl','lineitem.tbl','nation.tbl','orders.tbl','part.tbl','partsupp.tbl','region.tbl','supplier.tbl']
-    try: 
+    tables = ['customer','lineitem','nation','orders','part','partsupp','region','supplier']
+    try:
         for table in tables:
-            file_path = f'resources/db/postgres/tpch/{table}'
+            file_path = f'resources/db/postgres/tpch/{table}.tbl'
             with open(file_path, encoding='UTF-8', mode='r') as table_file:
-                table_name = table[:-4]
-                sql = f"COPY {table_name} FROM STDIN WITH (FORMAT CSV, DELIMITER '|', HEADER OFF, ENCODING 'UTF8');"
+                sql = f"COPY {table} FROM STDIN WITH (FORMAT CSV, DELIMITER '|', HEADER OFF, ENCODING 'UTF8');"
                 cur.copy_expert(sql, table_file)
         conn.commit()
     except Exception as e:
         conn.rollback()
         print(f"Error loading table data: {e}")
                 #### to be continued ####
-#load_tbl_data()
+load_tbl_data()
