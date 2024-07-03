87d759d4f105e8da988f07c16c86a5b5fc6480bb
fridtjof-damm <soenke.f.damm@campus.tu-berlin.de>
2024-07-03 11:13:32 +0200

    [ADD] DROP TABLES tpch psql

--- src/utils/psql_tpch_setup.py
+++ src/utils/psql_tpch_setup.py
@@ -7,6 +7,21 @@ db_params = {
     'host':'localhost',
     'port':'5432'
 }
+tables = ['customer','lineitem','nation','orders','part','partsupp','region','supplier']
+
+def drop_tables():
+    conn = pg.connect(**db_params)
+    conn.autocommit = True
+    cur = conn.cursor()
+    try:
+        for table in tables:
+            sql = f"DROP TABLE IF EXISTS {table};"
+            cur.execute(sql)
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        print(f"Error dropping table: {e}")
+#drop_tables()
 
 def load_tpch_schema(): 
     conn = pg.connect(**db_params)
@@ -23,7 +38,7 @@ def load_tpch_schema():
 def load_tbl_data():
     conn = pg.connect(**db_params)
     cur = conn.cursor()
-    tables = ['customer','lineitem','nation','orders','part','partsupp','region','supplier']
+
     try:
         for table in tables:
             file_path = f'resources/db/postgres/tpch/{table}.tbl'
