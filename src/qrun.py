e406cc5c046774cc46591c78e5b1299be9d1e2a9
Fridtjof Damm <fda@bdg.io>
2024-08-16 08:02:48 +0200

    [ADD] run picasso func

--- src/qrun.py
+++ src/qrun.py
@@ -34,3 +34,17 @@ def run_query_psql(cur, query_id, prefix):
             plans.append(json_str)
             statement_file.close()
         return plans
+    
+def run_query_picasso(cur, query_id, prefix):
+    with open(f'resources/queries_picasso/qt{query_id}.sql', encoding="UTF8") as statement_file:
+        template = statement_file.read()
+        queries = qg.generate_query(template, query_id)[0]
+        plans = []
+        for q in queries:
+            sql = prefix + q
+            cur.execute(sql)
+            json_plan = cur.fetchall()
+            json_str = json.dumps(json_plan, indent=4)
+            plans.append(json_str)
+            statement_file.close()
+        return plans
