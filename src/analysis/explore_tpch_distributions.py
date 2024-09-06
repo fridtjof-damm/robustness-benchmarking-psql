18651db7096b6dc47585736f00a9977b702d4a43
Fridtjof Damm <fda@bdg.io>
2024-09-06 15:44:22 -0400

    [add] tpch dsitributions exploration

--- src/analysis/explore_tpch_distributions.py
+++ src/analysis/explore_tpch_distributions.py
@@ -15,13 +15,12 @@ table = 'lineitem'
 attribute = 'l_extendedprice'
 
 
-
+# get stats min max cardinalities
 q_stat = f'''select count(*) as cardinality,
     min({attribute}) as min_val, 
     max({attribute}) as max_val
     from {table};
     '''
-
 cur.execute(q_stat)
 cardinality, min_val, max_val = cur.fetchone()
 
@@ -43,6 +42,8 @@ for val in values:
     queries_unique_vals.append(("{:.2f}".format(float(val)),query_uni))
 
 for query in queries_unique_vals:
+    queries = []
+    queries.append(query[1])
     cur.execute(query[1])
     selectivity_values.append(cur.fetchone()[0])
   #  cur.execute(q_unique_vals)
@@ -51,7 +52,6 @@ for query in queries_unique_vals:
   #  if selectivity in selectivties:
   #      selectivity_values.append((selectivity, val))
 # conn.close()
-
 print(selectivity_values)
 
 
