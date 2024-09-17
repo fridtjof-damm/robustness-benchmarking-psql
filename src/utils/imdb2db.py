42d0c83917723cb6386837e0dd1c67cc485f6de9
Fridtjof Damm <fda@bdg.io>
2024-09-17 15:40:49 -0400

    [ADD] parse imdb schema for job

---
+++ src/utils/imdb2db.py
@@ -0,0 +1,33 @@
+import gzip
+import psycopg2 as pg
+import os
+import src.utils.db_connector as dc
+
+# Join-Order Benchmark files
+gz_dir = 'Users/fridtjofdamm/Documents/job-data'
+job_schema = '/Users/fridtjofdamm/Documents/join-order-benchmark/schema.sql'
+
+def parse_schema_file(schema_path): 
+    tables = {}
+    with open(schema_path, 'r') as f:
+        for line in f:
+            if line.strip().startswith('CREATE TABLE'):
+                table_name = line.split()[2]
+                tables[table_name] = []
+            elif table_name and line.strip().startswith(')'):
+                table_name = None
+            elif table_name and ',' in line:
+                column = line.strip().split()[0]
+                tables[table_name].append(column)
+    len(tables) == 21
+    print('successfully parsed schema file')
+    return tables
+
+# get tables and columns from schema file
+tables = parse_schema_file(job_schema)
+
+# establish connection to database
+conn = dc.get_db_connection('job')
+cur = conn.cursor()
+
+print(tables['aka_name'])
