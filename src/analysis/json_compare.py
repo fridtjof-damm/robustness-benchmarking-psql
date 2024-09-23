77cf0725ebe6490efa07d92744808404be2fe977
Fridtjof Damm <fda@bdg.io>
2024-09-23 14:25:36 -0400

    [ADD] JOB Compare by Query ID

--- src/analysis/json_compare.py
+++ src/analysis/json_compare.py
@@ -1,80 +1,113 @@
 import json 
 import os
-import time
+import sys
+from itertools import combinations
 from deepdiff import DeepDiff
+from src.utils.utils import extract_number
+from datetime import datetime
 
-# tpch 
-#query_ids = [i for i in range(1,23) if i != 15]
-# for single query comparison , change func call!
-# target query format: 'q1' ,etc...
-#target_query = 'q2'
-#base_dir = 'results/tpch/qplans/'
-#dir = os.path.join(base_dir, target_query)
+def load_json(file_path) -> dict:
+    """
+    Load a JSON file from the given path.
+    """
+    with open(file_path, encoding='UTF-8', mode='r') as f:
+        return json.load(f)
 
-# job
-dir = 'results/job/qplans/'
+def compare_plans(plan1, plan2) -> dict:
+    """
+    Compare two query plans and return the differences.
+    """
+    diff = DeepDiff(plan1, plan2, verbose_level=2)
+    return diff
+
+def extract_key(path):
+    """
+    Extract the key from a path.
+    """
+    return path.split("']")[-2].split("['")[-1]
+
+def summarize_diff(diff, show_full_paths=True) -> str:
+    """
+    Summarize the differences between two query plans.
+    """
+    summary = []
+    if 'values_changed' in diff:
+        for path, change in diff['values_changed'].items():
+            key = path if show_full_paths else extract_key(path)
+            summary.append((path,f"Changed: {key}\nOld: {change['old_value']}\nNew: {change['new_value']}\n"))
+    if 'type_changes' in diff:
+        for path, change in diff['type_changes'].items():
+            key = path if show_full_paths else extract_key(path)
+            summary.append((path,f"Type changed: {key}\nOld: {change['old_type']} {change['old_value']}\nNew: {change['new_type']} {change['new_value']}\n"))
+    if 'dictionary_item_added' in diff:
+        for item in diff['dictionary_item_added']:
+            key = item if show_full_paths else extract_key(item)
+            summary.append((item,f"Added: {key}\n"))
+    if 'dictionary_item_removed' in diff:
+        for item in diff['dictionary_item_removed']:
+            key = item if show_full_paths else extract_key(item)
+            summary.append((item,f"Removed: {key}\n"))
+    summary.sort(key=lambda x: x[0])
+
+    return "\n".join(change for _, change in summary)
+
+def main() -> None:
+    """ 
+    Compare query plans for the same query from different runs.
+    """
+    # exact query number or None to compare all queries
+    query_number = None
+    # show the full paths 
+    show_full_paths = True
+    # using the job query plans
+    folder = 'results/job/qplans'
+    # create a logs folder if it does not exist
+    logs_folder = 'results/job/logs'
+    os.makedirs(logs_folder, exist_ok=True)
+
+    # get timestamp
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S") 
 
 
+    files = [f for f in os.listdir(folder) if f.endswith('.json')]
 
-def compare_json_files(json_file1, json_file2):
-    diff = DeepDiff(json_file1, json_file2, verbose_level=1)
-    return diff if diff else None
+    # comparing plans in groups of their prefix
+    # group files by prefix
+    groups = {}
+    for file in files:
+        number, suffix = extract_number(file)
+        if number not in groups:
+            groups[number] = []
+        groups[number].append(file)
 
-def process_json_files(dir):
-    results = {}
-    json_contents = {}
+    sorted_groups = sorted(groups.items())
 
-    # list all filenames 
-    files = []
-    for filename in os.listdir(dir):
-        if filename.endswith('.json'):
-            files.append(filename)
-    # load json files 
-    for filename in files:
-        filepath = os.path.join(dir, filename)
-        with open(filepath, encoding='UTF-8', mode='r') as f:
-            json_contents[filename] = json.load(f)
-            print("Success loading JSON file: ", filename)
-    # compare two files 
-    for i in range(0, len(files) - 1, 2):
-            file1 = files[i] 
-            file2 = files[i + 1]
-            print("Comparing", file1, "with", file2)
-            diff = compare_json_files(json_contents[file1], json_contents[file2])
-            if diff:
-                results[(file1, file2)] = diff
-    return results
+    if query_number is not None:
+        sorted_groups = [(num, group) for num, group in sorted_groups if num == query_number]
+        if not sorted_groups:
+            print(f"No plans found for query {query_number}.")
+            return  
 
-def print_results(comparison_results):
-    count = 0
-    if comparison_results:
-        for (file1,file2), diff in comparison_results.items():
-            count += 1
-            #print(f'Comparison between {file1} and {file2}:')
-            #print(f"Found differences: {diff}")
-            #print("Comparison success")
-            #print(f"="*42)
-            #print(f"Count: {count}")"""
-        print(count)
+    for number, group in sorted_groups:
+        print(f"\nComparing plans for groups {number}:")
+        for file1, file2 in  combinations(group, 2):
+            plan1 = load_json(os.path.join(folder, file1))
+            plan2 = load_json(os.path.join(folder, file2))
+            diff = compare_plans(plan1, plan2)
+            summary = summarize_diff(diff, show_full_paths)
 
-  
-    else: 
-        print(f"="*42) 
-        print(f"No differences found for Query {target_query[1:]}.")
-        print(f"="*42) 
-        
+            # write to log file
+            log_filename = f"comparison_{timestamp}_{number}_{file1.split('.')[0]}_{file2.split('.')[0]}.log"
+            log_path = os.path.join(logs_folder, log_filename)
 
-print_results(process_json_files(dir))
+            with open(log_path, 'w', encoding='UTF-8') as log_file:
+                log_file.write(f"Comparison performed on: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+                log_file.write(f"Comparison between {file1} and {file2}:\n")
+                log_file.write(summary if summary else "No significant differences found.")
 
-def compare_2_plans():
-    # paths
-    path1 = 'results/tpch/qplans/q9/q9_p27.json'
-    path2 = 'results/tpch/qplans/q9/q9_p31.json'
-    with open(path1, encoding='UTF-8', mode='r') as f1, open(path2, encoding='UTF-8', mode='r') as f2:   
-        json_file1 = json.load(f1)
-        json_file2 = json.load(f2)
-        diff = DeepDiff(json_file1, json_file2, verbose_level=1)
-        print(diff)
+            print(f"Comparison between {file1} and {file2}:")
+            print(summary if summary else "No significant differences found.")
 
-#compare_2_plans()
-
+if __name__ == '__main__':
+    main()
+ 
