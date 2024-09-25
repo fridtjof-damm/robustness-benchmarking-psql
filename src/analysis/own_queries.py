288faa1058669d72ecf97e19f7c6982e92a1d98b
Fridtjof Damm <fda@bdg.io>
2024-09-25 14:26:24 -0400

    [ADD] own queries analysis script

---
+++ src/analysis/own_queries.py
@@ -0,0 +1,39 @@
+import os
+import json
+import psycopg2 as pg
+import src.utils.db_connector as db_conn
+from src.analysis.qanalyze import simplify
+
+# prepare queries
+def prepare_queries() -> list[str]:
+    queries = []
+    query_ids = [1, 2, 3, 4, 6, 7]
+    with open('resources/queries_fd/job_1.sql', mode='r', encoding='UTF-8') as file:
+        template = file.read()
+        for i in query_ids:
+            sql = template.format(KIND = i)
+            queries.append(sql)
+    return queries
+
+def profile_queries() -> None:
+    conn = db_conn.get_db_connection('job')
+    cur = conn.cursor()
+    prefix = 'EXPLAIN (FORMAT JSON) '
+    plans = []
+    queries = prepare_queries()
+    for i, query in enumerate(queries):
+        cur.execute(prefix + query)
+        plan = cur.fetchall()
+        plan = simplify(plan)
+        plans.append((f'job_1_{i+1}', plan))
+    
+    # write plans to file
+    dir = f'results/fd/qplans/'
+    for plan in plans:
+        filename = os.path.join(dir, f'{plan[0]}.json')
+        with open(filename, 'w', encoding='UTF-8') as file:
+            file.write(json.dumps(plan[1], indent=4))
+            file.close()
+            print(f'success writing plan {plan[0]} to file')
+
+profile_queries()
