ba4c43e34c0f4e0741d924f1ada45959a538f624
Fridtjof Damm <fda@bdg.io>
2024-12-03 16:22:46 +0100

    complex

--- resources/query_examples/countries.py
+++ resources/query_examples/countries.py
@@ -3,34 +3,61 @@ import pandas as pd
 import numpy as np
 import random
 import math
+from faker import Faker
+from src.utils.db_connector import get_db_connection
 
-url = 'https://de.wikipedia.org/wiki/Liste_der_L%C3%A4nder_und_Territorien_nach_Einwohnerzahl'
-html = pd.read_html(url)
-df = max(html, key=len)
-# nur staat und einwohnerzahl
-df = df[['Staat', 'Einwohnerzahl (2024)']]
-# welt und eu rausnehemn 
-df = df[df['Staat'] != 'Welt']
-df = df[df['Staat'] != 'Europäische Union']
-# columns umbenenen
-df.columns = ['country', 'population']
-# tausender seperatoren removen
-df['population'] = df['population'].str.replace('.', '') 
-df['population'] = df['population'].astype(int)
-# durch tausend teilen wegen einfachheit
-#df['population'] = np.where(df['population'] < 1000,
-#                            df['population'],
-#                           df['population'].astype(int) // 1000)'
-df['population'] = df['population'].apply(lambda x: math.ceil(x/1000))
-# clean up country names 
-df['country'] = df['country'].str.replace('\[.*?\]', '')
-df['country'] = df['country'].str.strip()
-df['country'] = (df['country']
-                .str.replace('\[.*?\]', '', regex=True)      
-                .str.replace('\xa0', ' ', regex=True)        
-                .str.replace('\s+', ' ', regex=True)         
-                .str.strip()) 
-# reset index
-df = df.reset_index(drop=True)
-print(df.tail(10))
+# init faker
+fake = Faker()
 
+conn = get_db_connection('countries')
+cur = conn.cursor()
+
+cur.execute('''
+    SELECT country, count(country) FROM users GROUP BY country ORDER BY count(country) DESC;
+''')
+
+
+cur.execute("SELECT * FROM users;")
+current_data = cur.fetchall()
+
+
+cur.execute('''
+    CREATE TABLE users_extended (
+        id SERIAL PRIMARY KEY,
+        country VARCHAR(100),
+        capital VARCHAR(100),
+        president VARCHAR(100),
+        population INTEGER,
+        area_km2 INTEGER,
+        official_language VARCHAR(100),
+        currency VARCHAR(50),
+        random_text VARCHAR(100)
+    );
+''')
+print('table created')
+print('creating data...')
+
+batch_data = [(user_id, 
+        country, 
+        fake.city(),
+        fake.name(),
+        random.randint(1000, 1000000),
+        random.randint(1000, 1000000),
+        fake.language_name(),
+        fake.currency_code(),
+        fake.text(max_nb_chars=100)) for user_id, country in current_data]
+
+print('data created')
+
+print('starting to insert data...')
+
+cur.executemany('''
+    INSERT INTO users_extended (
+        id, country, capital, president, population, area_km2, official_language, currency, random_text)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);
+    ''', batch_data)
+  
+conn.commit()
+cur.close()
+conn.close()
+print("Done!")
