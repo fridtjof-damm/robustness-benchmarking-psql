0d55e81918f53fefa277b9a6e9bb3455bc8bf265
Fridtjof Damm <fda@bdg.io>
2024-12-31 12:02:44 +0100

    newly added tpch filter conditions

--- src/analysis/test_filter_conds.py
+++ src/analysis/test_filter_conds.py
@@ -18,56 +18,87 @@ def simplify_filter(filter_str, benchmark):
             condition = re.sub(r'::[a-zA-Z\s]+', '', condition)
             print("After removing type casts:", condition)  # Debug print
 
-            # Custom transformations for specific cases
-            # Transform (p_type LIKE '%TIN') to (p_type,'TIN')
-            condition = re.sub(
-                r'p_type LIKE \'%([^%]+)\'', r'(p_type,\'\1\')', condition)
-            # Transform (p_size = 1) to (p_size,1)
-            condition = re.sub(r'p_size = (\d+)', r'(p_size,\1)', condition)
-            # Transform (l_shipdate > '1995-03-27') to (l_shipdate ,'1995-03-27')
-            condition = re.sub(
-                r'l_shipdate > \'([^\']+)\'', r'(l_shipdate,\'\1\')', condition)
-            # Transform (c_mktsegment = 'HOUSEHOLD') to (c_mktsegment,'HOUSEHOLD')
-            condition = re.sub(
-                r'c_mktsegment = \'([^\']+)\'', r'(c_mktsegment,\'\1\')', condition)
-            # Transform (r_name = 'AFRICA') to (r_name,'AFRICA')
-            condition = re.sub(
-                r'r_name = \'([^\']+)\'', r'(r_name,\'\1\')', condition)
-            # Transform (o_orderdate >= '1993-01-01') to (o_orderdate,'1993-01-01')
-            condition = re.sub(
-                r'o_orderdate >= \'([^\']+)\'', r'(o_orderdate,\'\1\')', condition)
-            # Transform (o_orderdate <= '1996-12-31') to (o_orderdate,'1996-12-31')
-            condition = re.sub(
-                r'o_orderdate <= \'([^\']+)\'', r'(o_orderdate,\'\1\')', condition)
-            # Transform (o_orderdate < '1994-01-01 00:00:00') to (o_orderdate,'1994-01-01')
-            condition = re.sub(
-                r'o_orderdate < \'([^\']+)\s[^\']+\'', r'(o_orderdate,\'\1\')', condition)
-            # Transform (l_shipdate >= '1995-01-01') to (l_shipdate,'1995-01-01')
-            condition = re.sub(
-                r'l_shipdate >= \'([^\']+)\'', r'(l_shipdate,\'\1\')', condition)
-            # Transform (l_shipdate <= '1996-12-31') to (l_shipdate,'1996-12-31')
-            condition = re.sub(
-                r'l_shipdate <= \'([^\']+)\'', r'(l_shipdate,\'\1\')', condition)
-            # Transform (l_shipdate < '1993-02-01 00:00:00') to (l_shipdate,'1993-02-01')
-            condition = re.sub(
-                r'l_shipdate < \'([^\']+)\s[^\']+\'', r'(l_shipdate,\'\1\')', condition)
-            # Transform (n_name = 'ALGERIA') to (n_name,'ALGERIA')
-            condition = re.sub(
-                r'n_name = \'([^\']+)\'', r'(n_name,\'\1\')', condition)
-            # Transform (p_type = 'TIN') to (p_type,'TIN')
-            condition = re.sub(
-                r'p_type = \'([^\']+)\'', r'(p_type,\'\1\')', condition)
-            # Transform (p_brand = 'BRAND#11') to (p_brand,'BRAND#11')
-            condition = re.sub(
-                r'p_brand = \'([^\']+)\'', r'(p_brand,\'\1\')', condition)
-            # Transform (p_container = 'SM CASE') to (p_container,'SM CASE')
-            condition = re.sub(
-                r'p_container = \'([^\']+)\'', r'(p_container,\'\1\')', condition)
+            # Apply transformations based on specific patterns
+            match condition:
+                case _ if re.search(r'p_type LIKE', condition):
+                    # Transform (p_type LIKE '%TIN') to (p_type,'TIN')
+                    condition = re.sub(
+                        r'p_type LIKE \'%([^%]+)\'', r'(p_type,\'\1\')', condition)
+                case _ if re.search(r'p_size =', condition):
+                    # Transform (p_size = 1) to (p_size,1)
+                    condition = re.sub(r'p_size = (\d+)',
+                                       r'(p_size,\1)', condition)
+                case _ if re.search(r'l_shipdate >', condition):
+                    # Transform (l_shipdate > '1995-03-27') to (l_shipdate ,'1995-03-27')
+                    condition = re.sub(
+                        r'l_shipdate > \'([^\']+)\'', r'(l_shipdate,\'\1\')', condition)
+                case _ if re.search(r'c_mktsegment =', condition):
+                    # Transform (c_mktsegment = 'HOUSEHOLD') to (c_mktsegment,'HOUSEHOLD')
+                    condition = re.sub(
+                        r'c_mktsegment = \'([^\']+)\'', r'(c_mktsegment,\'\1\')', condition)
+                case _ if re.search(r'r_name =', condition):
+                    # Transform (r_name = 'AFRICA') to (r_name,'AFRICA')
+                    condition = re.sub(
+                        r'r_name = \'([^\']+)\'', r'(r_name,\'\1\')', condition)
+                case _ if re.search(r'o_orderdate >=', condition):
+                    # Transform (o_orderdate >= '1993-01-01') to (o_orderdate,'1993-01-01')
+                    condition = re.sub(
+                        r'o_orderdate >= \'([^\']+)\'', r'(o_orderdate,\'\1\')', condition)
+                case _ if re.search(r'o_orderdate <=', condition):
+                    # Transform (o_orderdate <= '1996-12-31') to (o_orderdate,'1996-12-31')
+                    condition = re.sub(
+                        r'o_orderdate <= \'([^\']+)\'', r'(o_orderdate,\'\1\')', condition)
+                case _ if re.search(r'o_orderdate <', condition):
+                    # Transform (o_orderdate < '1994-01-01 00:00:00') to (o_orderdate,'1994-01-01')
+                    condition = re.sub(
+                        r'o_orderdate < \'([^\']+)\s[^\']+\'', r'(o_orderdate,\'\1\')', condition)
+                case _ if re.search(r'l_shipdate >=', condition):
+                    # Transform (l_shipdate >= '1995-01-01') to (l_shipdate,'1995-01-01')
+                    condition = re.sub(
+                        r'l_shipdate >= \'([^\']+)\'', r'(l_shipdate,\'\1\')', condition)
+                case _ if re.search(r'l_shipdate <=', condition):
+                    # Transform (l_shipdate <= '1996-12-31') to (l_shipdate,'1996-12-31')
+                    condition = re.sub(
+                        r'l_shipdate <= \'([^\']+)\'', r'(l_shipdate,\'\1\')', condition)
+                case _ if re.search(r'l_shipdate <', condition):
+                    # Transform (l_shipdate < '1993-02-01 00:00:00') to (l_shipdate,'1993-02-01')
+                    condition = re.sub(
+                        r'l_shipdate < \'([^\']+)\s[^\']+\'', r'(l_shipdate,\'\1\')', condition)
+                case _ if re.search(r'n_name =', condition):
+                    # Transform (n_name = 'ALGERIA') to (n_name,'ALGERIA')
+                    condition = re.sub(
+                        r'n_name = \'([^\']+)\'', r'(n_name,\'\1\')', condition)
+                case _ if re.search(r'p_type =', condition):
+                    # Transform (p_type = 'TIN') to (p_type,'TIN')
+                    condition = re.sub(
+                        r'p_type = \'([^\']+)\'', r'(p_type,\'\1\')', condition)
+                case _ if re.search(r'p_brand =', condition):
+                    # Transform (p_brand = 'BRAND#11') to (p_brand,'BRAND#11')
+                    condition = re.sub(
+                        r'p_brand = \'([^\']+)\'', r'(p_brand,\'\1\')', condition)
+                case _ if re.search(r'p_container =', condition):
+                    # Transform (p_container = 'SM CASE') to (p_container,'SM CASE')
+                    condition = re.sub(
+                        r'p_container = \'([^\']+)\'', r'(p_container,\'\1\')', condition)
+                case _ if re.search(r'\(\(p_type\): : text ~~', condition):
+                    # Transform ((p_type): : text ~~ '%TIN': : text) to (p_type),'%TIN'
+                    condition = re.sub(
+                        r'\(\(p_type\): : text ~~ \'%([^%]+)\'', r'(p_type),\'\1\'', condition)
+                case _ if re.search(r'\(p_type\),\'[^\']+\': : text', condition):
+                    # Transform (p_type),'TIN': : text) to (p_type,'TIN')
+                    condition = re.sub(
+                        r'\(p_type\),\'([^\']+)\': : text\)', r'(p_type,\'\1\')', condition)
+
+            # Remove unnecessary escape characters from the transformed condition
+            condition = condition.replace("\\'", "'")
 
             transformed_conditions.append(condition)
 
         # Recombine the transformed conditions with commas
         filter_str = ','.join(transformed_conditions)
+
+        # Remove unnecessary outer parentheses from the entire filter string
+        filter_str = re.sub(r'^\((.*)\)$', r'\1', filter_str)
 
     return filter_str
 
